4283d6381d6fb55f75a2d599c835240f7d690800
Himanikuraware <kurawarehimani26@gmail.com>
2023-09-02 15:38:02 +0530

    Used Context API with typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,16 @@
-import { useState } from "react";
-
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
-import "./App.css";
 import NewTodo from "./components/NewTodo";
+import TodosContextProvider from "./store/todos-context";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  // const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
-
-  const addTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prev) => {
-      return prev.concat(newTodo);
-    });
-  };
-
-  const removeTodoHandler = (todoId: string) => {
-    setTodos((prev) => {
-      return prev.filter((todo) => todo.id !== todoId);
-    });
-  };
-
   return (
-    <div className="App">
-      <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
-    </div>
+    <TodosContextProvider>
+      <div className="App">
+        <NewTodo />
+        <Todos />
+      </div>
+    </TodosContextProvider>
   );
 }
 
