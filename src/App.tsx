06e0152dd60b020a64cc59e3d3497621c9bd4488
Himanikuraware <kurawarehimani26@gmail.com>
2023-09-02 14:18:17 +0530

    Added Data Model.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 import "./App.css";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
   return (
     <div className="App">
-      <Todos items={["Learn React", "Learn Typescript"]} />
+      <Todos items={todos} />
     </div>
   );
 }
