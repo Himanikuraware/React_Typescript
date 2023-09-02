a058a5bb7a805a6497e6c4d7bf932ab64cad0651
Himanikuraware <kurawarehimani26@gmail.com>
2023-09-02 14:50:25 +0530

    Worked with refs and function props.

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+    const enteredText = todoTextInputRef.current!.value; // ! it menas that we are sure that todoTextInputRef can't be null at that point.
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    props.onAddTodo(enteredText);
   };
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="todo">Todo Text</label>
-      <input type="text" id="todo" />
+      <input type="text" id="todo" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
